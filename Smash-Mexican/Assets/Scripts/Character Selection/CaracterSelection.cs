52bbbb2681c29b18a40d5d59315ed6b1fc8f0525
Pato1302 <patalvher@hotmail.com>
2022-11-22 21:16:59 -0600

    Start Character Selection

---
+++ Smash-Mexican/Assets/Scripts/Character Selection/CaracterSelection.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CaracterSelection : MonoBehaviour
+{
+
+    public void DrSimi()
+    {
+        PlayerPrefs.SetInt("Character", 1);
+    }
+
+    public void MamaLucha()
+    {
+        PlayerPrefs.SetInt("Character", 2);
+    }
+
+    public void SusanaDis()
+    {
+        PlayerPrefs.SetInt("Character", 3);
+    }
+
+    public void ChapulinColorado()
+    {
+        PlayerPrefs.SetInt("Character", 4);
+    }
+
+    public void Go()
+    {
+        //SceneManager.LoadScene("DesertScene_mobile");
+        //SceneManager.LoadScene("Cami Nv1", LoadSceneMode.Additive);
+        //SceneManager.LoadScene("Rami Nv1", LoadSceneMode.Additive);
+    }
+}
